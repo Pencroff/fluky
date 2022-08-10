8cab62a4ab575c72bb9a5d9d24cf785c30ab115c
Sergey Daniloff @Pencroff <pencroff@gmail.com>
2022-08-10 20:54:21 +0100

    refactoring stream implementations

--- container/rnd_stream.go
+++ container/rnd_stream.go
@@ -1,29 +1,19 @@
 package main
 
 import (
-	"encoding/binary"
-	"github.com/Pencroff/fluky/fluky_testing"
-	"github.com/Pencroff/fluky/rng"
 	"log"
+	"math/rand"
 	"os"
 )
 
 func main() {
-	butchSize := int(8 * fluky_testing.Size1Kb)
-	r := rng.NewPcgRng()
-	smB := make([]byte, 8)
-	b := make([]byte, butchSize)
-	itterNum := butchSize / 8
+	butchSize := 8 * 1024
+	bff := make([]byte, butchSize)
+	src := rand.NewSource(11111)
+	rnd := rand.New(src)
 	for true {
-		for i := 0; i < itterNum; i++ {
-			v := r.Uint64()
-			binary.LittleEndian.PutUint64(smB, v)
-			for j := 0; j < 8; j++ {
-				idx := 8*i + j
-				b[idx] = smB[j]
-			}
-		}
-		_, err := os.Stdout.Write(b)
+		rnd.Read(bff)
+		_, err := os.Stdout.Write(bff)
 		if err != nil {
 			log.Fatal(err)
 		}
