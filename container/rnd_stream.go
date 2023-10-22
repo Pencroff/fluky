4c9c3e1a558d301e99ab95120f824f7440ecd276
Pencroff <pencroff@gmail.com>
2023-10-22 15:34:45 +0100

    add custom note message

--- container/rnd_stream.go
+++ container/rnd_stream.go
@@ -3,11 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	rng "github.com/Pencroff/fluky/source"
 	"log"
 	"math/rand"
 	"os"
 	"reflect"
+	"time"
 )
+
+const notePeriodSec = 10
 
 func main() {
 	numbPtr := flag.Int64("seed", 0, "seed value")
@@ -15,17 +19,33 @@ func main() {
 	butchSize := 8 * 1024
 	bff := make([]byte, butchSize)
 
-	src := rand.NewSource(*numbPtr)
+	src := rng.NewSquirrel3x2Source(*numbPtr)
 
 	os.Stderr.WriteString(fmt.Sprintf("Seed: %d\n", *numbPtr))
 	os.Stderr.WriteString(fmt.Sprintf("Source: %s\n", reflect.TypeOf(src).String()))
 
+	start := time.Now()
+	tm := start.Add(time.Second * notePeriodSec)
+
 	rnd := rand.New(src)
+
+	sizeCnt := 0
+
 	for true {
 		rnd.Read(bff)
 		_, err := os.Stdout.Write(bff)
+		sizeCnt += butchSize
 		if err != nil {
 			log.Fatal(err)
 		}
+		n := time.Now()
+		if tm.After(n) {
+			continue
+		}
+		tm = tm.Add(time.Second * notePeriodSec)
+		fSize := float64(sizeCnt) / (1024 * 1024 * 1024)
+		since := n.Sub(start).Round(time.Second)
+		speed := float64(sizeCnt) / (1024 * 1024) / since.Seconds()
+		os.Stderr.WriteString(fmt.Sprintf("%s: %.2f Gb (%.2f Mb/s)\n", since, fSize, speed))
 	}
 }
